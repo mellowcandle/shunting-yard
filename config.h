597c2a86e743005630434163ddfe3fcefd8b1788
Brian Marshall <bmars@sdf.lonestar.org>
2011-12-20 19:40:47 -0800

    Move defines to config.h

---
+++ config.h
@@ -0,0 +1,12 @@
+/* Number of characters to allocate for floats (including the decimal point) */
+#define FLOAT_LENGTH 8
+
+/* Error types */
+#define ERROR_SYNTAX       1
+#define ERROR_SYNTAX_STACK 2
+#define ERROR_RIGHT_PAREN  3
+#define ERROR_LEFT_PAREN   4
+#define ERROR_UNRECOGNIZED 5
+
+/* For calls to error() with an unknown column number */
+#define NO_COL_NUM -2
