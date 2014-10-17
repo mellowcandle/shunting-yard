9c8a60eac3d27cd71f93ef4e0674edf57f6a2d2b
Brian Marshall <bmarsd@gmail.com>
2014-10-16 22:16:37 -0700

    Swap order of arguments in test macros

--- tests/shunting-yard.c
+++ tests/shunting-yard.c
@@ -11,137 +11,137 @@
 #include <CUnit/Basic.h>
 #include <stdlib.h>
 
-#define SY_ASSERT(expected, expression) \
-        SY_ASSERT_STATUS(OK, expression); \
-        CU_ASSERT_DOUBLE_EQUAL(expected, result, 10e-11)
+#define ASSERT_RESULT(expression, expected) \
+        ASSERT_STATUS(expression, OK); \
+        CU_ASSERT_DOUBLE_EQUAL(result, expected, 10e-11)
 
-#define SY_ASSERT_STATUS(expected, expression) \
+#define ASSERT_STATUS(expression, expected) \
         CU_ASSERT(shunting_yard(expression, &result) == expected)
 
 static double result = 0.0;
 
 static void test_addition() {
-    SY_ASSERT(4, "2+2");
-    SY_ASSERT(4, "2  +  2");
-    SY_ASSERT(4, "2+2.");
-    SY_ASSERT(13, "3 + (5 + 1 + (2 + 2))");
-    SY_ASSERT(42, "1+2+4+8+16 + 11");
-    SY_ASSERT(4.2, "2.1+2.1");
+    ASSERT_RESULT("2+2", 4);
+    ASSERT_RESULT("2  +  2", 4);
+    ASSERT_RESULT("2+2.", 4);
+    ASSERT_RESULT("3 + (5 + 1 + (2 + 2))", 13);
+    ASSERT_RESULT("1+2+4+8+16 + 11", 42);
+    ASSERT_RESULT("2.1+2.1", 4.2);
 }
 
 static void test_subtraction() {
-    SY_ASSERT(4, "8-4");
-    SY_ASSERT(5, "15-10");
-    SY_ASSERT(28, "27 - (10 - 11)");
-    SY_ASSERT(-16, "-5-11");
-    SY_ASSERT(1.6, "-(2-3.6)");
-    SY_ASSERT(-12, "(-5-7)");
+    ASSERT_RESULT("8-4", 4);
+    ASSERT_RESULT("15-10", 5);
+    ASSERT_RESULT("27 - (10 - 11)", 28);
+    ASSERT_RESULT("-5-11", -16);
+    ASSERT_RESULT("-(2-3.6)", 1.6);
+    ASSERT_RESULT("(-5-7)", -12);
 }
 
 static void test_multiplication() {
-    SY_ASSERT(26, "13 * 2");
-    SY_ASSERT(6.4, "3.2*2");
-    SY_ASSERT(55, "20*2*1.375");
-    SY_ASSERT(-9, "0.75*((2*-4)*1.5)");
-    SY_ASSERT(13.5, "27*0.5");
+    ASSERT_RESULT("13 * 2", 26);
+    ASSERT_RESULT("3.2*2", 6.4);
+    ASSERT_RESULT("20*2*1.375", 55);
+    ASSERT_RESULT("0.75*((2*-4)*1.5)", -9);
+    ASSERT_RESULT("27*0.5", 13.5);
 }
 
 static void test_division() {
-    SY_ASSERT(0.5, "1/2");
-    SY_ASSERT(0.555, "3.885 / 7");
-    SY_ASSERT(70, "(140/2)/0.5/2");
-    SY_ASSERT(47, "((517/4)/2/.25/.25)/22");
-    SY_ASSERT(86, "2987898/34743");
+    ASSERT_RESULT("1/2", 0.5);
+    ASSERT_RESULT("3.885 / 7", 0.555);
+    ASSERT_RESULT("(140/2)/0.5/2", 70);
+    ASSERT_RESULT("((517/4)/2/.25/.25)/22", 47);
+    ASSERT_RESULT("2987898/34743", 86);
 }
 
 static void test_modulus() {
-    SY_ASSERT(4, "10 % 6");
-    SY_ASSERT(2, "2+3 % 3");
-    SY_ASSERT(9, "6*5%21");
-    SY_ASSERT(10, "10%11");
-    SY_ASSERT(0, "5 %5");
-    SY_ASSERT(2.7, "5.7%3");
-    SY_ASSERT(1.1415926535898, "pi%2");
+    ASSERT_RESULT("10 % 6", 4);
+    ASSERT_RESULT("2+3 % 3", 2);
+    ASSERT_RESULT("6*5%21", 9);
+    ASSERT_RESULT("10%11", 10);
+    ASSERT_RESULT("5 %5", 0);
+    ASSERT_RESULT("5.7%3", 2.7);
+    ASSERT_RESULT("pi%2", 1.1415926535898);
 }
 
 static void test_exponentiation() {
-    SY_ASSERT(9, "3^2");
-    SY_ASSERT(0.01, "10^-2");
-    SY_ASSERT(16, "4^2");
-    SY_ASSERT(256, "2^8");
-    SY_ASSERT(390625, "5^(2^3)");
+    ASSERT_RESULT("3^2", 9);
+    ASSERT_RESULT("10^-2", 0.01);
+    ASSERT_RESULT("4^2", 16);
+    ASSERT_RESULT("2^8", 256);
+    ASSERT_RESULT("5^(2^3)", 390625);
 }
 
 static void test_factorials() {
-    SY_ASSERT(1, "1!");
-    SY_ASSERT(2, "2!");
-    SY_ASSERT(6, "3!");
-    SY_ASSERT(24, "4!");
-    SY_ASSERT(120, "5!");
-    SY_ASSERT(7, "3!+1");
+    ASSERT_RESULT("1!", 1);
+    ASSERT_RESULT("2!", 2);
+    ASSERT_RESULT("3!", 6);
+    ASSERT_RESULT("4!", 24);
+    ASSERT_RESULT("5!", 120);
+    ASSERT_RESULT("3!+1", 7);
 }
 
 static void test_functions() {
-    SY_ASSERT(32, "abs(-32)");
-    SY_ASSERT(12, "abs(-5-7)");
-    SY_ASSERT(1.1, "abs(-1.1)");
-    SY_ASSERT(10, "sqrt(100)");
-    SY_ASSERT(10, "SqRt(100)");
-    SY_ASSERT(10, "sqrt(sqrt(10000))");
-    SY_ASSERT(30, "sqrt(sqrt(10000) + 800)");
-    SY_ASSERT(42, "42 * cos(0)");
-    SY_ASSERT(-1, "(sin(0)*cos(0)*40*tan(0))-1");
-    SY_ASSERT(1, "log(10)");
-    SY_ASSERT(1, "lOG(10)");
-    SY_ASSERT(3, "lb(8)");
-    SY_ASSERT(1, "ln(e)");
-    SY_ASSERT(1, "Ln(E)");
-    SY_ASSERT(42, "log(10^42)");
-    SY_ASSERT(123, "lb(2^123)");
+    ASSERT_RESULT("abs(-32)", 32);
+    ASSERT_RESULT("abs(-5-7)", 12);
+    ASSERT_RESULT("abs(-1.1)", 1.1);
+    ASSERT_RESULT("sqrt(100)", 10);
+    ASSERT_RESULT("SqRt(100)", 10);
+    ASSERT_RESULT("sqrt(sqrt(10000))", 10);
+    ASSERT_RESULT("sqrt(sqrt(10000) + 800)", 30);
+    ASSERT_RESULT("42 * cos(0)", 42);
+    ASSERT_RESULT("(sin(0)*cos(0)*40*tan(0))-1", -1);
+    ASSERT_RESULT("log(10)", 1);
+    ASSERT_RESULT("lOG(10)", 1);
+    ASSERT_RESULT("lb(8)", 3);
+    ASSERT_RESULT("ln(e)", 1);
+    ASSERT_RESULT("Ln(E)", 1);
+    ASSERT_RESULT("log(10^42)", 42);
+    ASSERT_RESULT("lb(2^123)", 123);
 }
 
 static void test_variables() {
-    SY_ASSERT(-1, "cos(pi)");
-    SY_ASSERT(0, "tan(pi)");
-    SY_ASSERT(0, "Tan(PI)");
-    SY_ASSERT(1, "cos(tau)");
-    SY_ASSERT(1, "COS(TAU)");
-    SY_ASSERT(1, "cos(2pi)");
-    SY_ASSERT(1, "((2pi/tau)+(10pi))/(1+10pi)");
+    ASSERT_RESULT("cos(pi)", -1);
+    ASSERT_RESULT("tan(pi)", 0);
+    ASSERT_RESULT("Tan(PI)", 0);
+    ASSERT_RESULT("cos(tau)", 1);
+    ASSERT_RESULT("COS(TAU)", 1);
+    ASSERT_RESULT("cos(2pi)", 1);
+    ASSERT_RESULT("((2pi/tau)+(10pi))/(1+10pi)", 1);
 }
 
 static void test_precedence() {
-    SY_ASSERT(10, "6/3*5");
-    SY_ASSERT(12, "6+3*2");
-    SY_ASSERT(-100, "-10^2");
-    SY_ASSERT(100, "(-10)^2");
-    SY_ASSERT(101, "10^2+1");
-    SY_ASSERT(1.01, "10^-2+1");
-    SY_ASSERT(0.99, "-10^-2+1");
-    SY_ASSERT(0.02, "10^-2*2");
-    SY_ASSERT(20, "2+6/2*5+10/3-2/6");
-    SY_ASSERT(1000000, "10^3!");
-    SY_ASSERT(M_PI / 100, "10^-2pi");
-    SY_ASSERT(256, "2^2^3");
+    ASSERT_RESULT("6/3*5", 10);
+    ASSERT_RESULT("6+3*2", 12);
+    ASSERT_RESULT("-10^2", -100);
+    ASSERT_RESULT("(-10)^2", 100);
+    ASSERT_RESULT("10^2+1", 101);
+    ASSERT_RESULT("10^-2+1", 1.01);
+    ASSERT_RESULT("-10^-2+1", 0.99);
+    ASSERT_RESULT("10^-2*2", 0.02);
+    ASSERT_RESULT("2+6/2*5+10/3-2/6", 20);
+    ASSERT_RESULT("10^3!", 1000000);
+    ASSERT_RESULT("10^-2pi", M_PI / 100);
+    ASSERT_RESULT("2^2^3", 256);
 }
 
 static void test_errors() {
-    SY_ASSERT_STATUS(ERROR_SYNTAX, "2+*2");
-    SY_ASSERT_STATUS(ERROR_SYNTAX, "2**2");
-    SY_ASSERT_STATUS(ERROR_SYNTAX, "*1");
-    SY_ASSERT_STATUS(ERROR_SYNTAX, "2*.");
-    SY_ASSERT_STATUS(ERROR_SYNTAX, "2*2 3");
-    SY_ASSERT_STATUS(ERROR_SYNTAX, "2*2.3.4");
-    SY_ASSERT_STATUS(ERROR_SYNTAX, "pi2");
-    SY_ASSERT_STATUS(ERROR_OPEN_PARENTHESIS, "(2+2");
-    SY_ASSERT_STATUS(ERROR_OPEN_PARENTHESIS, "(2+2)+(2+2");
-    SY_ASSERT_STATUS(ERROR_CLOSE_PARENTHESIS, "(2+2))");
-    SY_ASSERT_STATUS(ERROR_UNRECOGNIZED, "2+&3");
-    SY_ASSERT_STATUS(ERROR_NO_INPUT, "");
-    SY_ASSERT_STATUS(ERROR_NO_INPUT, "       ");
-    SY_ASSERT_STATUS(ERROR_UNDEFINED_FUNCTION, "foo(2)");
-    SY_ASSERT_STATUS(ERROR_FUNCTION_ARGUMENTS, "sqrt()");
-    SY_ASSERT_STATUS(ERROR_UNDEFINED_CONSTANT, "foo");
+    ASSERT_STATUS("2+*2", ERROR_SYNTAX);
+    ASSERT_STATUS("2**2", ERROR_SYNTAX);
+    ASSERT_STATUS("*1", ERROR_SYNTAX);
+    ASSERT_STATUS("2*.", ERROR_SYNTAX);
+    ASSERT_STATUS("2*2 3", ERROR_SYNTAX);
+    ASSERT_STATUS("2*2.3.4", ERROR_SYNTAX);
+    ASSERT_STATUS("pi2", ERROR_SYNTAX);
+    ASSERT_STATUS("(2+2", ERROR_OPEN_PARENTHESIS);
+    ASSERT_STATUS("(2+2)+(2+2", ERROR_OPEN_PARENTHESIS);
+    ASSERT_STATUS("(2+2))", ERROR_CLOSE_PARENTHESIS);
+    ASSERT_STATUS("2+&3", ERROR_UNRECOGNIZED);
+    ASSERT_STATUS("", ERROR_NO_INPUT);
+    ASSERT_STATUS("       ", ERROR_NO_INPUT);
+    ASSERT_STATUS("foo(2)", ERROR_UNDEFINED_FUNCTION);
+    ASSERT_STATUS("sqrt()", ERROR_FUNCTION_ARGUMENTS);
+    ASSERT_STATUS("foo", ERROR_UNDEFINED_CONSTANT);
 }
 
 int main() {
